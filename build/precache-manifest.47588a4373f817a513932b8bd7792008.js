372c6ffb761fde786bb4695b5084296d57b7b44e
igor.shev <loyalsun94415@yandex.com>
2020-09-06 14:35:10 +0300

    update the codebase

---
+++ build/precache-manifest.47588a4373f817a513932b8bd7792008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0ca6faeefac90e62150e5e5c3831e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13c08eb38f3dc58529e",
+    "url": "/static/css/2.d54f7d81.chunk.css"
+  },
+  {
+    "revision": "ec2412fada79e4924873",
+    "url": "/static/css/main.a539cfa2.chunk.css"
+  },
+  {
+    "revision": "f13c08eb38f3dc58529e",
+    "url": "/static/js/2.d428d52f.chunk.js"
+  },
+  {
+    "revision": "c07df7ab3b60e7fba9f1f165045d7366",
+    "url": "/static/js/2.d428d52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2412fada79e4924873",
+    "url": "/static/js/main.2539e071.chunk.js"
+  },
+  {
+    "revision": "94c60667f29e604a0332",
+    "url": "/static/js/runtime-main.a2f5aeba.js"
+  }
+]);
