90f1eebb90469ed18a3070a0996013cc8c241e68
igor.shev <loyalsun94415@yandex.com>
2020-09-07 05:06:37 +0300

    hosting user

--- backend/settings.py
+++ backend/settings.py
@@ -115,8 +115,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'igrambilchester@gmail.com'
-EMAIL_HOST_PASSWORD = 'BillGates94415'
+EMAIL_HOST_USER = 'loyaltalent94415@gmail.com'
+EMAIL_HOST_PASSWORD = 'BillGates94415_new!@#'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # DATABASES = {
